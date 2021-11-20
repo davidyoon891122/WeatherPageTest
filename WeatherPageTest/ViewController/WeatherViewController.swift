ca7bfa5c3daf6ebcdfd69d5b23b5dab618576999
davidyoon <qqpo12@naver.com>
2021-11-20 12:07:25 +0900

    add weather Test labels, modified plist files for redirect mainViewController

--- WeatherPageTest/ViewController/WeatherViewController.swift
+++ WeatherPageTest/ViewController/WeatherViewController.swift
@@ -8,12 +8,85 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    // MARK: - Properties
+    
+    private let locationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "강남구"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let weatherDescription: UILabel = {
+        let label = UILabel()
+        label.text = "안개"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let degreeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10º"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let maxLabel: UILabel = {
+        let label = UILabel()
+        label.text = "최고: 16º"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let minLabel: UILabel = {
+        let label = UILabel()
+        label.text = "최저: 5º"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let hStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .red
+        return tableView
+    }()
+    
+    
+    
+    // MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        addSubviews()
+        setLayoutConstrains()
     }
     
+    private func addSubviews() {
+        view.addSubview(locationLabel)
+        view.addSubview(weatherDescription)
+        view.addSubview(degreeLabel)
+        view.addSubview(maxLabel)
+        view.addSubview(minLabel)
+        view.addSubview(hStackView)
+        view.addSubview(tableView)
+    }
+    
+    private func setLayoutConstrains() {
+        locationLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        locationLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
+    }
+    
+    
+    // MARK: - Handlers
 
 }
