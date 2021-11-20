8bc265e9dd3eb27304fb467886169363550024ef
davidyoon <qqpo12@naver.com>
2021-11-20 10:27:28 +0900

    set skeleton layout for weather Page

--- WeatherPageTest/ViewController/ViewController.swift
+++ WeatherPageTest/ViewController/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    
+    // MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    // MARK: - Handlers
+    
 
 }
 
---
+++ WeatherPageTest/ViewController/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherViewController.swift
+//  WeatherPageTest
+//
+//  Created by David Yoon on 2021/11/20.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
