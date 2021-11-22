a572a588f3f9d229cd8ce28e43f0fd95713ba8d7
davidyoon <qqpo12@naver.com>
2021-11-22 15:47:52 +0900

    set weatherBoard components layout

--- WeatherPageTest/ViewController/WeatherViewController.swift
+++ WeatherPageTest/ViewController/WeatherViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 class WeatherViewController: UIViewController {
     // MARK: - Properties
     
+    private let weatherBoardVerticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.backgroundColor = .orange
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let weatherStatusByTimeHorizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.backgroundColor = .yellow
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let weatherForecaseByDayTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .green
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    
     private let locationLabel: UILabel = {
         let label = UILabel()
         label.text = "강남구"
+        label.font = UIFont.systemFont(ofSize: 20)
+//        label.layer.borderWidth = 2
+//        label.layer.borderColor = UIColor.blue.cgColor
+        label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -20,13 +49,16 @@ class WeatherViewController: UIViewController {
     private let weatherDescription: UILabel = {
         let label = UILabel()
         label.text = "안개"
+        label.textAlignment = .center
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let degreeLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 50)
+        label.font = UIFont.boldSystemFont(ofSize: 70)
+        label.textAlignment = .center
         label.text = "10º"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -76,35 +108,57 @@ class WeatherViewController: UIViewController {
     }
     
     private func addSubviews() {
-        view.addSubview(locationLabel)
-        view.addSubview(weatherDescription)
-        view.addSubview(degreeLabel)
-        view.addSubview(maxLabel)
-        view.addSubview(minLabel)
-        view.addSubview(hStackView)
-        view.addSubview(tableView)
+        view.addSubview(weatherBoardVerticalStackView)
+        view.addSubview(weatherStatusByTimeHorizontalStackView)
+        view.addSubview(weatherForecaseByDayTableView)
+        
+        weatherBoardVerticalStackView.addSubview(locationLabel)
+        weatherBoardVerticalStackView.addSubview(weatherDescription)
+        weatherBoardVerticalStackView.addSubview(degreeLabel)
+        weatherBoardVerticalStackView.addSubview(maxLabel)
+        weatherBoardVerticalStackView.addSubview(minLabel)
+        
     }
     
     private func setLayoutConstrains() {
-        locationLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-        locationLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        weatherBoardVerticalStackView.topAnchor.constraint(equalTo: super.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        weatherBoardVerticalStackView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        weatherBoardVerticalStackView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        weatherBoardVerticalStackView.heightAnchor.constraint(equalToConstant: 300).isActive = true
         
-        weatherDescription.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 5).isActive = true
-        weatherDescription.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        weatherStatusByTimeHorizontalStackView.topAnchor.constraint(equalTo: weatherBoardVerticalStackView.bottomAnchor).isActive = true
+        weatherStatusByTimeHorizontalStackView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        weatherStatusByTimeHorizontalStackView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        weatherStatusByTimeHorizontalStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
-        degreeLabel.topAnchor.constraint(equalTo: weatherDescription.bottomAnchor, constant: 5).isActive = true
-        degreeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        degreeLabel.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        weatherForecaseByDayTableView.topAnchor.constraint(equalTo: weatherStatusByTimeHorizontalStackView.bottomAnchor).isActive = true
+        weatherForecaseByDayTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        weatherForecaseByDayTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        weatherForecaseByDayTableView.heightAnchor.constraint(equalToConstant: 500).isActive = true
+        
+        locationLabel.topAnchor.constraint(equalTo: super.view.safeAreaLayoutGuide.topAnchor, constant: 60).isActive = true
+        locationLabel.leftAnchor.constraint(equalTo: super.view.leftAnchor).isActive = true
+        locationLabel.rightAnchor.constraint(equalTo: super.view.rightAnchor).isActive = true
+        locationLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        weatherDescription.topAnchor.constraint(equalTo: locationLabel.bottomAnchor).isActive = true
+        weatherDescription.leftAnchor.constraint(equalTo: super.view.leftAnchor).isActive = true
+        weatherDescription.rightAnchor.constraint(equalTo: super.view.rightAnchor).isActive = true
+        weatherDescription.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        degreeLabel.topAnchor.constraint(equalTo: weatherDescription.bottomAnchor).isActive = true
+        degreeLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        degreeLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        degreeLabel.heightAnchor.constraint(equalToConstant: 90).isActive = true
         
         maxLabel.topAnchor.constraint(equalTo: degreeLabel.bottomAnchor).isActive = true
-        //maxLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         maxLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        maxLabel.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        maxLabel.widthAnchor.constraint(equalToConstant: view.frame.width/2 - 2).isActive = true
         
         minLabel.topAnchor.constraint(equalTo: degreeLabel.bottomAnchor).isActive = true
-        //minLabel.leftAnchor.constraint(equalTo: maxLabel.rightAnchor).isActive = true
+        minLabel.leftAnchor.constraint(equalTo: maxLabel.rightAnchor, constant: 2).isActive = true
         minLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        minLabel.widthAnchor.constraint(equalToConstant: 180).isActive = true
         
     }
     
